e050d282ce74be2509f1062a5e9e97931efa3e13
carlrey9 <48773188+carlrey9@users.noreply.github.com>
2022-01-30 11:57:22 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/my_team_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_team_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/util/colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class MyColors {
+  static const primaryColor = Color(0xff3dbfe8);
+}
